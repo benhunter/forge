4ca676f3454fd1fe6da7b8e8c1ca10f57fa7b126
Ben Hunter <code@benhunter.me>
2026-01-18 21:49:20 -0600

    Functioning implementation of PlayerControllerAiSimple

---
+++ forge-ai/src/test/java/forge/ai/simple/Helpers.java
@@ -0,0 +1,27 @@
+package forge.ai.simple;
+
+import forge.game.Game;
+import forge.game.GameRules;
+import forge.game.GameType;
+import forge.game.Match;
+import forge.game.player.RegisteredPlayer;
+import forge.util.Localizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Helpers {
+    public static Game createGame() {
+        // Create a minimal Game object to pass to the method
+        List<RegisteredPlayer> players = new ArrayList<>();
+        GameType gameType = GameType.Constructed;
+        GameRules rules = new GameRules(gameType);
+        Match match = new Match(rules, players, "Test Match");
+        return new Game(players, rules, match);
+    }
+
+    static void initializeLocalizer() {
+        Localizer localizer = Localizer.getInstance();
+        localizer.initialize("en-US", "../forge-gui/res/languages/"); // TODO: remove bad hardcoded path
+    }
+}
